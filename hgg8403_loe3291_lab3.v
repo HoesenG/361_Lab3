6b0a7c30c8a2fc787f6cbe4e4bb367b2b420be4b
HoesenG <houxuanguo2024@u.northwestern.edu>
2024-11-05 16:41:56 -0600

    updated alu but still outputs xxx

--- hgg8403_loe3291_lab3.v
+++ hgg8403_loe3291_lab3.v
@@ -20,7 +20,7 @@
 `define OPCODE_JAL        7'b1101111
 `define OPCODE_JALR       7'b1100111
 `define OPCODE_IMM        7'b0010011
-`define OPCODE_MULT_DIV   7'b0110011
+`define OPCODE_MUL_DIV    7'b0110011
 
 module SingleCycleCPU(halt, clk, rst);
    output halt;
@@ -45,7 +45,10 @@ module SingleCycleCPU(halt, clk, rst);
    
    // Only support R-TYPE ADD and SUB
    assign halt = invalid_op;
-   assign invalid_op = !((opcode == `OPCODE_COMPUTE) && (funct3 == `FUNC_ADD) &&
+   assign invalid_op = !(((opcode == `OPCODE_COMPUTE) || (opcode == `OPCODE_AUIPC)
+             || (opcode == `OPCODE_BRANCH) || (opcode == `OPCODE_IMM) || (opcode == `OPCODE_JAL)
+             || (opcode == `OPCODE_JALR) || (opcode == `OPCODE_LOAD) || (opcode == `OPCODE_LUI) 
+             || (opcode == `OPCODE_MUL_DIV) || (opcode == `OPCODE_STORE)) && (funct3 == `FUNC_ADD) &&
 		      ((funct7 == `AUX_FUNC_ADD) || (funct7 == `AUX_FUNC_SUB)));
      
    // System State 
@@ -89,79 +92,95 @@ module ExecutionUnit(out, opA, opB, imm, func, auxFunc, opcode);
    input [6:0] auxFunc, opcode;
 
    wire [`WORD_WIDTH-1:0] addsub, slli, srli, srai,
-                        logicAnd, logicOr, logicXor, compLT, compLTU,
+                        logicAnd, logicOr, logicXor, slti, sltiu,
                         mul, mulh, mulhsu, mulhu, div, divu, rem, remu,
                         lui, auipc, jal, jalr, branch;
 
-   assign addsub = (auxFunc == 7'b0100000) ? (opA - opB) : (opA + opB);
-   assign slli = opA << opB[4:0];
-   assign srli = opA >> opB[4:0];
-   assign srai = $signed(opA) >>> opB[4:0];
+   // 默认输出值
+assign out = 32'hXXXXXXXX;
 
-   assign logicAnd = opA & opB;
-   assign logicOr = opA | opB;
-   assign logicXor = opA ^ opB;
+// R-Type指令
+assign addsub = (opcode == `OPCODE_COMPUTE && func == 3'b000 && auxFunc == `AUX_FUNC_ADD) ? (opA + opB) :
+                (opcode == `OPCODE_COMPUTE && func == 3'b000 && auxFunc == `AUX_FUNC_SUB) ? (opA - opB) : 32'hXXXXXXXX;
 
-   assign compLT = ($signed(opA) < $signed(opB)) ? 1 : 0;
-   assign compLTU = (opA < opB) ? 1 : 0;
+assign slli = (opcode == `OPCODE_COMPUTE && func == 3'b001) ? (opA << opB[4:0]) : 32'hXXXXXXXX;
+assign slti = (opcode == `OPCODE_COMPUTE && func == 3'b010) ? (($signed(opA) < $signed(opB)) ? 1 : 0) : 32'hXXXXXXXX;
+assign sltiu = (opcode == `OPCODE_COMPUTE && func == 3'b011) ? ((opA < opB) ? 1 : 0) : 32'hXXXXXXXX;
+assign logicXor = (opcode == `OPCODE_COMPUTE && func == 3'b100) ? (opA ^ opB) : 32'hXXXXXXXX;
+assign srli = (opcode == `OPCODE_COMPUTE && func == 3'b101 && auxFunc[5] == 0) ? (opA >> opB[4:0]) : 32'hXXXXXXXX;
+assign srai = (opcode == `OPCODE_COMPUTE && func == 3'b101 && auxFunc[5] == 1) ? ($signed(opA) >>> opB[4:0]) : 32'hXXXXXXXX;
+assign logicOr = (opcode == `OPCODE_COMPUTE && func == 3'b110) ? (opA | opB) : 32'hXXXXXXXX;
+assign logicAnd = (opcode == `OPCODE_COMPUTE && func == 3'b111) ? (opA & opB) : 32'hXXXXXXXX;
 
-   assign mul = opA * opB;
-   assign mulh = ($signed(opA) * $signed(opB)) >>> 32;
-   assign mulhsu = ($signed(opA) * opB) >>> 32;
-   assign mulhu = (opA * opB) >>> 32;
-   assign div = $signed(opA) / $signed(opB);  // Signed division
-   assign divu = opA / opB;                   // Unsigned division
-   assign rem = $signed(opA) % $signed(opB);  // Signed remainder
-   assign remu = opA % opB;                   // Unsigned remainder
+// I-Type指令
+assign addi = (opcode == `OPCODE_IMM && func == 3'b000) ? (opA + imm) : 32'hXXXXXXXX;
+assign slti = (opcode == `OPCODE_IMM && func == 3'b010) ? (($signed(opA) < $signed(imm)) ? 1 : 0) : 32'hXXXXXXXX;
+assign sltiu = (opcode == `OPCODE_IMM && func == 3'b011) ? ((opA < imm) ? 1 : 0) : 32'hXXXXXXXX;
+assign xori = (opcode == `OPCODE_IMM && func == 3'b100) ? (opA ^ imm) : 32'hXXXXXXXX;
+assign ori = (opcode == `OPCODE_IMM && func == 3'b110) ? (opA | imm) : 32'hXXXXXXXX;
+assign andi = (opcode == `OPCODE_IMM && func == 3'b111) ? (opA & imm) : 32'hXXXXXXXX;
+assign slli_imm = (opcode == `OPCODE_IMM && func == 3'b001) ? (opA << imm[4:0]) : 32'hXXXXXXXX;
+assign srli_imm = (opcode == `OPCODE_IMM && func == 3'b101 && auxFunc[5] == 0) ? (opA >> imm[4:0]) : 32'hXXXXXXXX;
+assign srai_imm = (opcode == `OPCODE_IMM && func == 3'b101 && auxFunc[5] == 1) ? ($signed(opA) >>> imm[4:0]) : 32'hXXXXXXXX;
 
-   assign lui = imm << 12;
-   assign auipc = opA + imm;
-   assign jal = opA + imm;
-   assign jalr = (opA + imm) & ~1;
+// 特殊指令
+assign lui = (opcode == `OPCODE_LUI) ? (imm << 12) : 32'hXXXXXXXX;
+assign auipc = (opcode == `OPCODE_AUIPC) ? (opA + imm) : 32'hXXXXXXXX;
+assign jal = (opcode == `OPCODE_JAL) ? (opA + imm) : 32'hXXXXXXXX;
+assign jalr = (opcode == `OPCODE_JALR) ? ((opA + imm) & ~1) : 32'hXXXXXXXX;
 
-   assign branch = (func == 3'b000 && (opA == opB)) ? 1 :  // beq
-                        (func == 3'b001 && (opA != opB)) ? 1 :  // bne
-                        (func == 3'b100 && ($signed(opA) < $signed(opB))) ? 1 :  // blt
-                        (func == 3'b101 && ($signed(opA) >= $signed(opB))) ? 1 :  // bge
-                        (func == 3'b110 && (opA < opB)) ? 1 :  // bltu
-                        (func == 3'b111 && (opA >= opB)) ? 1 :  // bgeu
-                        0;
+// Branch条件
+assign branch = (opcode == `OPCODE_BRANCH && func == 3'b000) ? ((opA == opB) ? 1 : 0) :  // BEQ
+                (opcode == `OPCODE_BRANCH && func == 3'b001) ? ((opA != opB) ? 1 : 0) :  // BNE
+                (opcode == `OPCODE_BRANCH && func == 3'b100) ? (($signed(opA) < $signed(opB)) ? 1 : 0) :  // BLT
+                (opcode == `OPCODE_BRANCH && func == 3'b101) ? (($signed(opA) >= $signed(opB)) ? 1 : 0) :  // BGE
+                (opcode == `OPCODE_BRANCH && func == 3'b110) ? ((opA < opB) ? 1 : 0) :  // BLTU
+                (opcode == `OPCODE_BRANCH && func == 3'b111) ? ((opA >= opB) ? 1 : 0) : 32'hXXXXXXXX;  // BGEU
 
-   // Final Output Selection Based on Opcode and Func
-   assign out = (opcode == `OPCODE_COMPUTE) ? 
-                    ((func == 3'b000) ? addSub : 
-                     (func == 3'b001) ? shiftLeft :
-                     (func == 3'b010) ? compLT : 
-                     (func == 3'b011) ? compLTU :
-                     (func == 3'b100) ? logicXor : 
-                     (func == 3'b101) ? (auxFunc[5] ? shiftRightArith : shiftRight) : 
-                     (func == 3'b110) ? logicOr : 
-                     (func == 3'b111) ? logicAnd : 32'hXXXXXXXX) :
-                (opcode == `OPCODE_IMM) ? 
-                    ((func == 3'b000) ? (opA + imm) : 
-                     (func == 3'b010) ? (($signed(opA) < $signed(imm)) ? 1 : 0) : 
-                     (func == 3'b011) ? ((opA < imm) ? 1 : 0) : 
-                     (func == 3'b100) ? (opA ^ imm) : 
-                     (func == 3'b110) ? (opA | imm) : 
-                     (func == 3'b111) ? (opA & imm) : 
-                     (func == 3'b001) ? (opA << imm[4:0]) : 
-                     (func == 3'b101) ? (auxFunc[5] ? ($signed(opA) >>> imm[4:0]) : (opA >> imm[4:0])) :
-                     32'hXXXXXXXX) :
-                (opcode == `OPCODE_LUI) ? lui :
-                (opcode == `OPCODE_AUIPC) ? auipc :
-                (opcode == `OPCODE_JAL) ? jal :
-                (opcode == `OPCODE_JALR) ? jalr :
-                (opcode == `OPCODE_BRANCH) ? branch :
-                (opcode == `OPCODE_LOAD || opcode == `OPCODE_STORE) ? (opA + imm) :
-                (opcode == `OPCODE_MUL_DIV) ?
-                    ((func == 3'b000) ? mul :
-                     (func == 3'b001) ? mulh :
-                     (func == 3'b010) ? mulhsu :
-                     (func == 3'b011) ? mulhu :
-                     (func == 3'b100) ? div :
-                     (func == 3'b101) ? divu :
-                     (func == 3'b110) ? rem :
-                     (func == 3'b111) ? remu : 32'hXXXXXXXX) :
-                32'hXXXXXXXX; // Default case for unsupported operations
+// Load和Store指令的地址计算
+assign load_store_addr = (opcode == `OPCODE_LOAD || opcode == `OPCODE_STORE) ? (opA + imm) : 32'hXXXXXXXX;
+
+// Multiply/Divide指令
+assign mul_result = (opcode == `OPCODE_MUL_DIV && func == 3'b000) ? (opA * opB) : 32'hXXXXXXXX;
+assign mulh_result = (opcode == `OPCODE_MUL_DIV && func == 3'b001) ? (($signed(opA) * $signed(opB)) >>> 32) : 32'hXXXXXXXX;
+assign mulhsu_result = (opcode == `OPCODE_MUL_DIV && func == 3'b010) ? (($signed(opA) * opB) >>> 32) : 32'hXXXXXXXX;
+assign mulhu_result = (opcode == `OPCODE_MUL_DIV && func == 3'b011) ? ((opA * opB) >>> 32) : 32'hXXXXXXXX;
+assign div_result = (opcode == `OPCODE_MUL_DIV && func == 3'b100) ? ($signed(opA) / $signed(opB)) : 32'hXXXXXXXX;
+assign divu_result = (opcode == `OPCODE_MUL_DIV && func == 3'b101) ? (opA / opB) : 32'hXXXXXXXX;
+assign rem_result = (opcode == `OPCODE_MUL_DIV && func == 3'b110) ? ($signed(opA) % $signed(opB)) : 32'hXXXXXXXX;
+assign remu_result = (opcode == `OPCODE_MUL_DIV && func == 3'b111) ? (opA % opB) : 32'hXXXXXXXX;
+
+// 最终输出选择
+assign out = (addsub != 32'hXXXXXXXX) ? addsub :
+             (slli != 32'hXXXXXXXX) ? slli :
+             (slti != 32'hXXXXXXXX) ? slti :
+             (sltiu != 32'hXXXXXXXX) ? sltiu :
+             (logicXor != 32'hXXXXXXXX) ? logicXor :
+             (srli != 32'hXXXXXXXX) ? srli :
+             (srai != 32'hXXXXXXXX) ? srai :
+             (logicOr != 32'hXXXXXXXX) ? logicOr :
+             (logicAnd != 32'hXXXXXXXX) ? logicAnd :
+             (addi != 32'hXXXXXXXX) ? addi :
+             (slti != 32'hXXXXXXXX) ? slti :
+             (sltiu != 32'hXXXXXXXX) ? sltiu :
+             (xori != 32'hXXXXXXXX) ? xori :
+             (ori != 32'hXXXXXXXX) ? ori :
+             (andi != 32'hXXXXXXXX) ? andi :
+             (slli_imm != 32'hXXXXXXXX) ? slli_imm :
+             (srli_imm != 32'hXXXXXXXX) ? srli_imm :
+             (srai_imm != 32'hXXXXXXXX) ? srai_imm :
+             (lui != 32'hXXXXXXXX) ? lui :
+             (auipc != 32'hXXXXXXXX) ? auipc :
+             (jal != 32'hXXXXXXXX) ? jal :
+             (jalr != 32'hXXXXXXXX) ? jalr :
+             (branch != 32'hXXXXXXXX) ? branch :
+             (load_store_addr != 32'hXXXXXXXX) ? load_store_addr :
+             (mul_result != 32'hXXXXXXXX) ? mul_result :
+             (mulh_result != 32'hXXXXXXXX) ? mulh_result :
+             (mulhsu_result != 32'hXXXXXXXX) ? mulhsu_result :
+             (mulhu_result != 32'hXXXXXXXX) ? mulhu_result :
+             (div_result != 32'hXXXXXXXX) ? div_result :
+             (divu_result != 32'hXXXXXXXX) ? divu_result :
+             (rem_result != 32'hXXXXXXXX);
 
 endmodule // ExecutionUnit
